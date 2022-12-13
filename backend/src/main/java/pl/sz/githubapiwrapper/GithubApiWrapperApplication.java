c0eb90a78459332b2a3dd5b4697d0a2f0225d13f
Szymon Zalewski <83140027+szymonzalewski2406@users.noreply.github.com>
2022-12-13 15:11:19 +0100

    initial commit backend

---
+++ backend/src/main/java/pl/sz/githubapiwrapper/GithubApiWrapperApplication.java
@@ -0,0 +1,13 @@
+package pl.sz.githubapiwrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubApiWrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubApiWrapperApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/pl/sz/githubapiwrapper/GithubApiWrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sz.githubapiwrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApiWrapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
